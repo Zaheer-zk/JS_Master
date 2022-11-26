19bdcfa8ea970109119f17210e47431814734cd9
Zaheer khan <newzk16117541@gmail.com>
2022-11-26 07:32:01 +0530

    Adding Higher order Function

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+// HIgher Order Function
+
+const callback = function (str) {
+  return str.replace('/ /g', '').toLowerCase();
+};
+
+const temp = function (str, fn) {
+  console.log(`String :   ${fn(str)}`);
+  console.log(fn.name); // returns callback function name which is callback here
+};
+
+temp('Javascript', callback);
+
+//  Function return function
+
+const greetHelper = message => {
+  return name => {
+    console.log(`${message} ${name}`);
+  };
+};
+
+greetHelper('Hello')('Zaheer');
